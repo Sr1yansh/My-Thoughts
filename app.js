e229467268821f77869c275549df96d8ba5d06b9
Shriyansh <shriyansh.verma98@gmail.com>
2021-05-19 00:50:06 +0530

    Process form created and errors handli. Empty fields handling.

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const exphbs = require('express-handlebars');
 const port = 5000;
 const mongoose = require('mongoose');
+const bodyParser =require('body-parser');
+
+
 // Map global promise -  get rid of warning
 mongoose.Promise = global.Promise; 
+
+
 //Connect to mongoose.
 mongoose.connect('mongodb://localhost/vidjot-dev', { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected....'))
@@ -21,6 +26,11 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.get('/',(req,res)=> {
   const title = 'Welcome';
   res.render('index',{
@@ -28,13 +38,41 @@ app.get('/',(req,res)=> {
   });
 })
 
+//About Route
 app.get('/about',(req,res)=> {
   res.render('about');
 })
 
+//Add Idea Form
 app.get('/ideas/add',(req,res)=> {
   res.render('ideas/add');
 })
+
+//Process Form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title){
+    errors.push({text: 'Please add a title.'})
+  }
+  
+  if(!req.body.details){
+    errors.push({text: 'Please add some details.'})
+  }
+
+  if(errors.length > 0){
+    res.render('ideas/add',{
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    })
+  }
+
+  else{
+    res.send('Submitted!')
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
